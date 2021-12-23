25690dab2c6b01f594b865c4df3cc8ae4a83a1a7
AlexanderBousbaine <a.bousbaine@lancaster.ac.uk>
2021-12-23 00:44:17 +0000

    added results

---
+++ ResultAnalysis.py
@@ -0,0 +1,45 @@
+import pandas
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+
+def load(files):
+    li = []
+    all_files = glob.glob(files)
+    for filename in all_files:
+        # print(filename)
+        df = pandas.read_csv(filename, header=None)
+        df = df.iloc[:, -1]
+        li.append(df)
+        
+    frame = pandas.concat(li, axis=1, ignore_index=True)
+    #frame.columns = ["gen1", "gen2", "gen3", "gen4", "gen5", "gen6", "gen7", "gen8", "gen9", "gen10"]
+    return frame
+
+if __name__ == "__main__":
+    # Read in results
+    filesPath = "./Results/*/*"
+    
+    # Loads fitness columns from all files
+    # Gives dataframe with 10 columns and 12 rows - each column is a generation
+    fitnesses = load(filesPath)
+    #fitnesses.insert(0, "Gens", ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10", "g11", "g12"])
+    
+    bestFitnesses = pandas.DataFrame(columns=["Generation", "Fitness"])
+    for v in fitnesses.columns: #[0,1,2,3,4,5,6,7,8,9]:
+        bfit = fitnesses.iloc[:, v].min()
+        bestFitnesses.loc[len(bestFitnesses)] = [v+1, bfit]
+    
+    bestFitnesses.plot(x="Generation", y="Fitness",grid=True,title="Best Fitness from each Generation")
+    plt.show()
+    
+    #plotData = pandas.DataFrame(columns = ["Generation", "Fitness_Values"])
+    
+    #for g in range(0, 9):
+    #    plotData.loc[len(plotData)] = [g, fitnesses[g]]
+    
+    #print(plotData)
+    
+    #fitnesses.plot.scatter(x="Gens", y=9, grid=True)
+    #plt.show()
+    
